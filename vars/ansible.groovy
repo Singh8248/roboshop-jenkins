b39fe1b8b2d6233fd2652c6adae2581da738a747
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-20 08:50:31 +0530

    Update roboshop-jenkins/

--- vars/ansible.groovy
+++ vars/ansible.groovy
@@ -17,15 +17,45 @@ def call() {
 
     stages {
 
+      stage('CheckOut Code - DEV') {
+        when {
+          expression {
+            ENV == "dev"
+          }
+        }
+        steps {
+          dir('ANSIBLE') {
+            git branch: 'dev', url: 'https://github.com/raghudevopsb64/roboshop-ansible.git'
+          }
+        }
+      }
+
+      stage('CheckOut Code - PROD') {
+        when {
+          expression {
+            ENV == "prod"
+          }
+        }
+        steps {
+          dir('ANSIBLE') {
+            git branch: 'main', url: 'https://github.com/raghudevopsb64/roboshop-ansible.git'
+          }
+        }
+      }
+
       stage('Create Instance') {
         steps {
-          sh 'bash create-ec2-with-env.sh ${COMPONENT} ${ENV}'
+          dir('ANSIBLE') {
+            sh 'bash create-ec2-with-env.sh ${COMPONENT} ${ENV}'
+          }
         }
       }
 
       stage('Run Ansible Playbook') {
         steps {
-          sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+          dir('ANSIBLE') {
+            sh 'ansible-playbook -i inv roboshop.yml -e ENV=${ENV} -e ansible_user=${SSH_USR} -e ansible_password=${SSH_PSW} -e HOST=${COMPONENT} -e ROLE_NAME=${COMPONENT}'
+          }
         }
       }
 
