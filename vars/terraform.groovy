69a736c77be80c3b9982a223be6fd3a6d59b7362
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-30 07:56:03 +0530

    Update roboshop-jenkins/

---
+++ vars/terraform.groovy
@@ -0,0 +1,38 @@
+def call() {
+  pipeline {
+    agent any
+
+    options {
+      ansiColor('xterm')
+    }
+
+    parameters {
+      string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+    }
+
+    environment {
+      SSH = credentials('SSH')
+    }
+
+    stages {
+
+      stage('Terraform INIT') {
+        steps {
+          sh '''
+            terraform init -backend-config=env-${ENV}/backend.tfvars
+          '''
+        }
+      }
+
+      stage('Terraform Apply') {
+        steps {
+          sh '''
+            terraform init -var-file=env-${ENV}/${ENV}.tfvars
+          '''
+        }
+      }
+
+    }
+
+  }
+}
