89cf8c5d3dbf5a6eb281cb41a228c82b2f5a7c9d
Raghu K <raghuk.vit@gmail.com>
2022-06-28 08:11:29 +0530

    Update roboshop-jenkins/

--- vars/multibranchTest.groovy
+++ vars/multibranchTest.groovy
@@ -1,12 +1,12 @@
 def call() {
   node{
-    if(BRANCH_NAME == "main") {
+    if(BRANCH_NAME != "main") {
       stage('test') {
         sh 'echo hello'
       }
     }
 
-    if(BRANCH_NAME != "main") {
+    if(BRANCH_NAME == "main") {
       stage('prod') {
         sh 'echo hello'
       }
