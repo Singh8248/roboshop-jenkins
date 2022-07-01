e5ccba5a54999d1e6a7d40519f9a4cfac164070b
Raghu K <raghuk.vit@gmail.com>
2022-07-01 07:53:36 +0530

    Update roboshop-jenkins/

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,5 +1,13 @@
 def call() {
+  if (env.APPTYPE == "maven" ) {
+  sh """
+    sonar-scanner -Dsonar.host.url=http://172.31.92.121:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${env.COMPONENT} -Dsonar.java.binaries=target/
+  """
+  }
+  else {
   sh """
     sonar-scanner -Dsonar.host.url=http://172.31.92.121:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${env.COMPONENT}
   """
+  }
+
 }
