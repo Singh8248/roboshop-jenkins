1640f10ea9cf2507fd7ee1b9335cddf90c1c2cc3
Raghu K <raghuk.vit@gmail.com>
2022-07-01 07:49:01 +0530

    Update roboshop-jenkins/

--- vars/compileCode.groovy
+++ vars/compileCode.groovy
@@ -2,5 +2,10 @@ def compile() {
   if (env.APPTYPE == "nodejs" ) {
     sh 'echo "NodeJS does not require Compilation"'
   }
+
+  if (env.APPTYPE == "maven" ) {
+    sh 'mvn clean compile'
+  }
+
 }
 
