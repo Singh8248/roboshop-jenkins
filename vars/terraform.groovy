75debfb1e2af4a512ac44844ac36c7f0db1deef8
Raghu K <raghuk.vit@gmail.com>
2022-07-04 08:46:46 +0530

    Update roboshop-jenkins/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -8,6 +8,7 @@ def call() {
 
     parameters {
       string(name: 'ENV', defaultValue: '', description: 'Which Environment?')
+      string(name: 'VERSION', defaultValue: '', description: 'Which Version?')
     }
 
     environment {
@@ -27,7 +28,7 @@ def call() {
       stage('Terraform Apply') {
         steps {
           sh '''
-            terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars
+            terraform apply -auto-approve -var-file=env-${ENV}/${ENV}.tfvars -var VERSION=${VERSION}
           '''
         }
       }
