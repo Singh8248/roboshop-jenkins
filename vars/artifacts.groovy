e65df941bf976e00551202a094d0a6b43bb49c01
Raghu K <raghuk.vit@gmail.com>
2022-07-06 08:06:19 +0530

    Update roboshop-jenkins/

--- vars/artifacts.groovy
+++ vars/artifacts.groovy
@@ -39,5 +39,8 @@ def ami() {
   sh """
     terraform init 
     terraform apply -auto-approve -var APP_VERSION=${TAG_NAME}
+    terraform state rm 'module.ami.aws_ami_from_instance.ami'
+    terraform destroy -auto-approve -var APP_VERSION=${TAG_NAME}
   """
 }
+
